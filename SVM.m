531e1e7603de733f616b0c167b8e7f808cb8db47
rhit-franklkj <franklkj@rose-hulman.edu>
2026-07-18 19:47:42 -0400

    mostly finding example images for the discussion
    this might be the worst code imaginable

--- SVM.m
+++ SVM.m
@@ -1,6 +1,5 @@
 % Training the SVM 
 
-
 % Load in the image data 
 test_nonsunset = readmatrix("features\test_nonsunset.csv"); 
 test_sunset = readmatrix("features\test_sunset.csv"); 
@@ -13,22 +12,23 @@ val_sunset = readmatrix("features\val_sunset.csv");
 
 % Concatenate the nonsunset and sunset matrices 
 % Assign labels of +1 to sunsets and -1 to nonsunsets 
-X_train = cat(1, train_nonsunset, train_sunset); 
-y_train = cat(1, ones(size(train_nonsunset, 1), 1) * -1, ones(size(train_sunset, 1), 1)); 
-
-X_val = cat(1, val_nonsunset, val_sunset); 
-y_val = cat(1, ones(size(val_nonsunset, 1), 1) * -1, ones(size(val_sunset, 1), 1)); 
+% Since we're doing cross-validation, I don't need a dedicated validation
+% set 
+X_train = cat(1, train_nonsunset, train_sunset, val_nonsunset, val_sunset); 
+y_train = cat(1, ones(size(train_nonsunset, 1), 1) * -1, ones(size(train_sunset, 1), 1), ones(size(val_nonsunset, 1), 1) * -1, ones(size(val_sunset, 1), 1)); 
 
 X_test = cat(1, test_nonsunset, test_sunset); 
 y_test= cat(1, ones(size(test_nonsunset, 1), 1) * -1, ones(size(test_sunset, 1), 1)); 
 
-C_range = 5:1:15;
-sigma_range = 15:1:25; 
-[C, sigma] = GridSearchCV(X_train, y_train, C_range, sigma_range); 
+% C_range = 1:1:12;
+% sigma_range = 15:1:25; 
+% [C, sigma] = GridSearchCV(X_train, y_train, C_range, sigma_range); 
+
+C = 3;
+sigma = 17; 
 
 disp_stat("Best Box Constraint:", C); 
 disp_stat("Best Kernel Scale:", sigma); 
-
 
 net = fitcsvm(X_train,y_train,'KernelFunction','rbf', 'Standardize',true, ...
     'ClassNames',[-1, 1], 'BoxConstraint', C, 'KernelScale', sigma); 
@@ -36,12 +36,11 @@ net = fitcsvm(X_train,y_train,'KernelFunction','rbf', 'Standardize',true, ...
 pct_sv = size(net.SupportVectors, 1) / size(X_train, 1); 
 disp_stat("Percent Support Vectors:", pct_sv); 
 
-% Predict on the validation set -- I don't need it for its usual purpose so
-% I can save the test set for later 
-[pred, dist] = predict(net, X_val); 
+% Predict on the test set 
+[pred, dist] = predict(net, X_test); 
+% analyze_performance(pred, dist, y_test);
 
-analyze_performance(pred, dist, y_val);
-
+find_example_images(pred, dist(:, 2), y_test); 
 
 function [best_C, best_sigma] = GridSearchCV(X_train, y_train, C_grid, sigma_grid)
 n_folds = 5; 
@@ -61,7 +60,6 @@ acc_grid = zeros(size(C_grid, 2), size(sigma_grid, 2));
 
 for i = 1:size(C_grid, 2) % iterate over the values of C 
     for j = 1:size(sigma_grid, 2)% iterate over the values of sigma 
-        fprintf("C =  %d, sigma = %d\n", C_grid(1, i), sigma_grid(1, j)); 
 
         acc_sum = 0; 
 
@@ -79,7 +77,7 @@ for i = 1:size(C_grid, 2) % iterate over the values of C
             % Train model 
             net = fitcsvm(X_train_folds, y_train_folds,'KernelFunction','rbf', 'Standardize',true,'ClassNames',[-1, 1], 'BoxConstraint', C_grid(1, i), 'KernelScale', sigma_grid(1, j));
 
-            % Predict on training set 
+            % Predict on validation fold 
             [pred, dist] = predict(net, X_valid_fold);
 
             % Add validation accuracy to running total 
@@ -88,6 +86,8 @@ for i = 1:size(C_grid, 2) % iterate over the values of C
         end
         % Divide running total by number of folds and save it to the grid 
         acc_grid(i, j) = acc_sum / n_folds; 
+        fprintf("C =  %d, sigma = %d, accuracy = %d\n", C_grid(1, i), sigma_grid(1, j), acc_sum / n_folds); 
+
     end
 end
 
@@ -177,4 +177,149 @@ function analyze_performance(pred, dist, y_test)
     fpr = fp / (fp + tn); 
     disp_stat("True Positive Rate: ", tpr); 
     disp_stat("False Positive Rate: ", fpr); 
+end
+
+function find_example_images(pred, dist, y_test)
+    % Y_test has 500 nonsunsets then 498 sunsets in that order 
+    y_test_nonsunset = y_test(1:500);
+    y_test_sunset = y_test(500:size(y_test,1));
+    
+    nonsunset = imageDatastore("images\test\nonsunset"); 
+    sunset = imageDatastore("images\test\sunset\"); 
+
+    dist 
+    min_tn_idx = 1; 
+    max_tn_idx = 1; 
+    min_tn_dist = max(dist); 
+    max_tn_dist = min(dist); 
+
+    min_fp_idx = 1; 
+    max_fp_idx = 1; 
+    min_fp_dist = max(dist); 
+    max_fp_dist = min(dist); 
+
+    min_tp_idx = 1; 
+    max_tp_idx = 1; 
+    min_tp_dist = max(dist); 
+    max_tp_dist = 0; 
+
+    min_fn_idx = 1; 
+    max_fn_idx = 1; 
+    min_fn_dist = max(dist); 
+    max_fn_dist = 0; 
+
+    % Nonsunsets 
+    for i = 1:500      
+        % Correct - True Negatives 
+        if(pred(i) == y_test_nonsunset(i))
+            % Farthest from margin (most negative) 
+            if(dist(i) < min_tn_dist)
+                min_tn_dist = dist(i); 
+                min_tn_idx = i; 
+            end
+            % Closest to margin (least negative) 
+            if(max_tn_dist < dist(i))
+                max_tn_dist = dist(i);
+                max_tn_idx = i; 
+            end
+        end
+        % Incorrect - False Positives 
+        if(pred(i) ~= y_test_nonsunset(i))
+            % Closest to margin (least positive) 
+            if(dist(i) < min_fp_dist)
+                min_fp_dist = dist(i); 
+                min_fp_idx = i; 
+            end
+            % Farthest from margin (most positive) 
+            if(max_fp_dist < dist(i))
+                max_fp_dist = dist(i);
+                max_fp_idx = i; 
+            end
+
+        end
+    end
+
+    % Sunsets 
+    for i = 1:498      
+        % Correct - True Positives 
+        if(pred(500 + i) == y_test_sunset(i)) % 500 + i to skip the nonsunsets 
+            % Closest to margin (least positive) 
+            if(dist(500 + i) < min_tp_dist)
+                min_tp_dist = dist(500 + i); 
+                min_tp_idx = i; 
+            end
+            % Farthest from margin (most positive) 
+            if(max_tp_dist < dist(500 + i))
+                max_tp_dist = dist(500 + i);
+                max_tp_idx = i; 
+            end
+        end
+        % Incorrect - False Negatives 
+        if(pred(500 + i) ~= y_test_sunset(i))
+            % Closest to margin (least negative) 
+            if(dist(500 + i) < min_fn_dist)
+                min_fn_dist = dist(500 + i); 
+                min_fn_idx = i; 
+            end
+            % Farthest from margin (most negative) 
+            if(max_fn_dist < dist(500 + i))
+                max_fn_dist = dist(500 + i);
+                max_fn_idx = i; 
+            end
+
+        end
+    end
+
+    figure(3); 
+
+    subplot(2, 4, 1); 
+    hold on; 
+    title(sprintf("TP Distance = %.2f", max_tp_dist)); 
+    imshow(readimage(sunset, max_tp_idx)); 
+    hold off;
+
+    subplot(2, 4, 2); 
+    hold on; 
+    title(sprintf("TP Distance = %.2f", min_tp_dist)); 
+    imshow(readimage(sunset, min_tp_idx)); 
+    hold off; 
+
+    subplot(2, 4, 3); 
+    hold on; 
+    title(sprintf("FN Distance = %.2f", min_fn_dist)); 
+    imshow(readimage(sunset, min_fn_idx)); 
+    hold off; 
+
+    subplot(2, 4, 4); 
+    hold on; 
+    title(sprintf("FN Distance = %.2f", max_fn_dist)); 
+    imshow(readimage(sunset, max_fn_idx)); 
+    hold off; 
+
+    subplot(2, 4, 5); 
+    hold on; 
+    title(sprintf("FP Distance = %.2f", max_fp_dist)); 
+    imshow(readimage(nonsunset, max_fp_idx)); 
+    hold off; 
+
+
+    subplot(2, 4, 6); 
+    hold on; 
+    title(sprintf("FP Distance = %.2f", min_fp_dist)); 
+    imshow(readimage(nonsunset, min_fp_idx)); 
+    hold off; 
+
+    subplot(2, 4, 7); 
+    hold on; 
+    title(sprintf("TN Distance = %.2f", max_tn_dist)); 
+    imshow(readimage(nonsunset, max_tn_idx)); 
+    hold off;
+
+    subplot(2, 4, 8); 
+    hold on; 
+    title(sprintf("TN Distance = %.2f", min_tn_dist)); 
+    imshow(readimage(nonsunset, min_tn_idx)); 
+    hold off; 
+
+
 end
